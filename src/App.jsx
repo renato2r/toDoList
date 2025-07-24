6234eba91ce76b758322ee91bf2527951a4abd2b
Renato Martins dos Santos <renato2r@gmail.com>
2025-07-23 18:59:57 -0700

    feat: add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
+// App.jsx
 import "./App.css";
+import TodoList from "./TodoList"; // Importe seu componente TodoList
+import TodoForm from "./TodoForm"; // Importe seu componente TodoForm
 
 function App() {
-  const todos = [
-    { id: 1, title: "review resources" },
-    { id: 2, title: "take notes" },
-    { id: 3, title: "code out app" },
-  ];
+  // O array 'todos' foi movido para TodoList, então não precisa mais estar aqui
+  // const todos = [
+  //   { id: 1, title: "review resources" },
+  //   { id: 2, title: "take notes" },
+  //   { id: 3, title: "code out app" },
+  // ];
+
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      {/* Apenas renderize o TodoList, sem passar props de 'todos' */}
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
